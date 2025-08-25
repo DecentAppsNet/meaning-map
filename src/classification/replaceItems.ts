1c1b104e69781d98a1be86b9e872e11137a352ef
Erik Hermansen <info@seespacelabs.com>
2025-08-25 10:57:40 -0700

    um

--- src/classification/replaceItems.ts
+++ src/classification/replaceItems.ts
@@ -1,3 +1,12 @@
+import Tagger from 'wink-pos-tagger';
+
+let thePosTagger:Tagger|null = null;
+
+function _getPosTagger() {
+  if (!thePosTagger) thePosTagger = new Tagger();
+  return thePosTagger;
+}
+
 export function replaceItems(text:string):string {
   return text; // TODO
 }
